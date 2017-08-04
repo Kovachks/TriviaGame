fb58fb3b76b4183b99cd2a445e6f87388b1593f9
unknown <kovachks90@gmail.comgit config --global kovachks90 Keith>
2017-08-03 22:33:58 -0400

    updated

--- assets/javascript/app3.js
+++ assets/javascript/app3.js
@@ -10,15 +10,15 @@ var incorrectSelection = 0;
 
 var correctSelection = 0;
 
-var questionArray = ["Who is the only Cleveland Cavalier to win an MVP award while on the Cavs?", "Who has hit the most homeruns as a Cleveland Indian?", "Who is the leading rusher for the Cleveland Browns>"];
+var questionArray = ["Who is the only Cleveland Cavalier to win an MVP award while on the Cavs?", "Who has hit the most homeruns as a Cleveland Indian?", "Who is the leading rusher for the Cleveland Browns"];
 
-var answerGif = ["test","src='assets/images/lebronJames.gif'", "src='assets/images/jimthome.gif'"]
+var answerGif = ["test","src='assets/images/lebronJames.gif'", "src='assets/images/jimthome.gif'", "src='assets/images/jimBrown.gif"]
 
 var buttonArray = [];
 
-// var buttonArray = ["<button data-correct='yes'> Lebron James</button>", "<button data-correct='no'>Austin Carr</button>","<button data-correct='no'>James Worthy</button>","<button data-correct='no'>Mark Price</button>",
-// 				   "<button data-correct='no'> Carlos Santana</button>", "<button data-correct='no'>Jackie Robinson</button>","<button data-correct='yes'>Jim Thome</button>","<button data-correct='no'>Kenny Lofton</button>",
-// 				   "<button data-correct='yes'>Jim Brown</button>","<button data-correct='no'>Peyton Hillis</button>","<button data-correct='no'>Leroy Kelly</button>","<button data-correct='no'>Earnest <Byner></Byner></button>"]
+ var buttonArray = ["<button data-correct='yes'> Lebron James</button>", "<button data-correct='no'>Austin Carr</button>","<button data-correct='no'>James Worthy</button>","<button data-correct='no'>Mark Price</button>",
+ 				   "<button data-correct='no'> Carlos Santana</button>", "<button data-correct='no'>Jackie Robinson</button>","<button data-correct='yes'>Jim Thome</button>","<button data-correct='no'>Kenny Lofton</button>",
+ 				   "<button data-correct='yes'>Jim Brown</button>","<button data-correct='no'>Peyton Hillis</button>","<button data-correct='no'>Leroy Kelly</button>","<button data-correct='no'>Earnest Byner</button>"]
 
 var correctAnswer = ["test" ,"Lebron James", "Jim Thome", "Jim Brown"]
 
@@ -40,43 +40,33 @@ function run() {
 		userSelection = $(this).attr("data-correct");
 	});
 
-	console.log('user selection is: ', userSelection);
 	intervalId = setInterval(decrement, 1000)
 	function decrement() {
 		number = number - 1;
 		$("#timer").html("<p>Time Remaining: " + number + " seconds</p>")
-			if(buttonArray.length){
-				if (number === 25 || userSelection == 'no'){
+				if (number === 0 || userSelection == 'no'){
 					userSelection = '';
 					incorrectSelection+=1;
-					// you got it wrong
-					console.log(userSelection);
 					removeQuestion();
 					answerObtained();
 					setTimeout(removeAnswer, 3000);
 					setTimeout(questionGen, 3000);
 					number = 33;
-					console.log(count);
 				} else if (userSelection == 'yes'){
+					userSelection = '';					
 					correctSelection+=1;
 					removeQuestion();
 					answerObtained();
 					setTimeout(removeAnswer, 3000);
 					setTimeout(questionGen, 3000);
 					number = 33;
-				}	else {
-					console.log('coding is fun');
+				}	else if (buttonArray === []) {
+
 				}
-			} else {
-				removeQuestion();
-				removeAnswer();
-				triviaContent.append("<div><p>You got " + correctSelection + " correct</p></div>");
-				triviaContent.append("<div><p>You got " + incorrectSelection + " incorrect</p></div>");
 			}
 		}
 		
 
-	}
 
 function questionGen () {
 	var questionDiv = $("<div class='questionClass'>"+ questionArray[count] +"</div>")
